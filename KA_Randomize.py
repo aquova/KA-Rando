de4b16330c28ddf6fd36f27184068f321c9666a0
Aquova <abrick912@gmail.com>
2017-05-24 16:22:57 -0500

    Version 1.00 complete, just need bug testing

--- KA_Randomize.py
+++ KA_Randomize.py
@@ -13,39 +13,70 @@ def getAbsPath(realPath):
     filename = os.path.join(currentPath, realPath)
     return filename
 
-# Reads and stores the hex values from enemy_list.txt
-# Contains the hex values of valid enemies
-def getAbilityValues():
-    abilities = open(getAbsPath("tables/ability_list.txt"), 'r')
-    ability_values = []
-    for line in abilities:
-        ability_values.append(line.split(" ")[0])
-    abilities.close()
-    return ability_values
+def readHexValues(fileName):
+    file = open(getAbsPath(fileName), 'r')
+    values = []
+    for line in file:
+        values.append(line.split(" ")[0])
+    file.close()
+    return values
 
-# Reads and stores the hex values from enemy_locations.txt
-# Contains the hex addresses of abilities in the ROM
-def getAbilityMap():
-    locations = open(getAbsPath("tables/ability_locations.txt"), 'r')
-    ability_rom_locs = []
-    for line in locations:
-        ability_rom_locs.append(line.split(" ")[0])
-    locations.close()
-    return ability_rom_locs
+# # Reads and stores the hex values from enemy_list.txt
+# # Contains the hex values of valid enemies
+# def getAbilityValues():
+#     abilities = open(getAbsPath("tables/ability_list.txt"), 'r')
+#     ability_values = []
+#     for line in abilities:
+#         ability_values.append(line.split(" ")[0])
+#     abilities.close()
+#     return ability_values
+#
+# # Reads and stores the hex values from enemy_locations.txt
+# # Contains the hex addresses of abilities in the ROM
+# def getAbilityLoc():
+#     locations = open(getAbsPath("tables/ability_locations.txt"), 'r')
+#     ability_rom_locs = []
+#     for line in locations:
+#         ability_rom_locs.append(line.split(" ")[0])
+#     locations.close()
+#     return ability_rom_locs
 
 if __name__ == "__main__":
-    print("Thanks for using the Kirby's Adventure Randomizer, version %s" % VERSION)
-    rom_name = raw_input("Give the name of the ROM (must be in same folder as this program): ")
-    rom_name = getAbsPath(rom_name)
-    KA_seed = raw_input("Now give a seed to be used (or leave blank): ")
-    random.seed(KA_seed)
-    rom = open(rom_name, 'rb').read()
-    for item in getAbilityMap():
-        address = int(item, 16)
-        rand_ind = random.randint(0,len(getAbilityValues()) - 1)
-        new_enemy = getAbilityValues()[rand_ind]
-        new_enemy = chr(int(new_enemy,16))
-        rom = rom[:address] + new_enemy + rom[(address + 1):]
-    new_rom = open(rom_name.split(".")[0] + "_" + KA_seed + ".nes", 'w')
-    new_rom.write(rom)
-    new_rom.close()
+    try:
+        print("Thanks for using the Kirby's Adventure Randomizer, version %s" % VERSION)
+        rom_name = raw_input("Give the name of the ROM (must be in same folder as this program): ")
+        rom_name = getAbsPath(rom_name)
+
+        # If seed is blank, generate a random seed
+        # Python will generate one by default, but you can't access it
+        KA_seed = raw_input("Now give a seed to be used (or leave blank): ")
+        if KA_seed == "":
+            KA_seed = random.randint(0, 999999999)
+        random.seed(KA_seed)
+
+        ability_values = readHexValues("tables/ability_list.txt")
+        ability_locations = readHexValues("tables/ability_enemies_locations.txt")
+
+        star_rod = raw_input("Should the Star Rod be an available ability? (Y/N): ")
+        neutral = raw_input("Should enemies without abilities also be randomized? (Y/N): ")
+        if star_rod.upper() == 'Y':
+            # Add 0x18, the value of Star Rod
+            ability_values.append(str(18))
+
+        if neutral.upper() == 'Y':
+            # Add in locations of enemies w/o abilities
+            neutral_locations = readHexValues("tables/neutral_enemies_locations.txt")
+            ability_locations.extend(neutral_locations)
+
+        rom = open(rom_name, 'rb').read()
+        for item in ability_locations:
+            address = int(item, 16)
+            rand_ind = random.randint(0,len(ability_values) - 1)
+            new_enemy = ability_values[rand_ind]
+            new_enemy = chr(int(new_enemy,16))
+            rom = rom[:address] + new_enemy + rom[(address + 1):]
+        new_rom = open(rom_name.split(".")[0] + "_" + str(KA_seed) + ".nes", 'w')
+        new_rom.write(rom)
+        new_rom.close()
+    except Exception, e:
+        print("ERROR: %s" % e)
